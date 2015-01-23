435d18aa1894ac39891821c540ecae7e0a803b93
Gerold Cheatham <geroldcheatham@gmail.com>
2015-01-23 11:51:20 -0800

    test commit

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,1 +1,29 @@
-// TODO
+game.PlayerEntity = me.Entity.extend({
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+                image: 64,
+                width: 64,
+                height: 64,
+                spritewidth: "64",
+                spriteheight: "64",
+                getShape: function() {
+                    return(new me.Rect(0, 0, 64, 64)).toPolygon();
+                }
+        }]);
+    
+        this.body.setVelocity(5, 0);
+    
+    },
+    
+    update: function() {
+        if(me.input.isKeyPressed("right")){
+            
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+        }else{
+            this.body.vel.x = 0;
+        }
+        
+        this.body.update(delta);
+        return true
+    }
+});
