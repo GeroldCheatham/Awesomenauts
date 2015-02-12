126b0cac1907c427a5ed520650ea90550ff545b3
Gerold Cheatham <geroldcheatham@gmail.com>
2015-02-12 11:46:59 -0800

    14-17 without comments

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -12,6 +12,10 @@ game.PlayerEntity = me.Entity.extend({
         }]);
     
         this.body.setVelocity(5, 20);
+        this.facing = "right";
+        this.now = new Date().getTime();
+        this.lastHit = this.now  ;
+        this.lastAttack = new Date().getTime();
         me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
         
         this.renderable.addAnimation("idle", [78]);
@@ -23,14 +27,15 @@ game.PlayerEntity = me.Entity.extend({
     },
     
     update: function(delta) {
-        if(me.input.isKeyPressed("right")){
-            
+        this.now = new Date().getTime();
+        if(me.input.isKeyPressed("right")){          
             this.body.vel.x += this.body.accel.x * me.timer.tick;
-            
+            this.facing = "right";
             this.flipX(true);
         }
         else if(me.input.isKeyPressed("left")){
             this.body.vel.x -= this.body.accel.x * me.timer.tick;
+            this.facing = "left";
             this.flipX(false);
         }
         else{
@@ -42,13 +47,13 @@ game.PlayerEntity = me.Entity.extend({
             this.jumping = false;
         }
         
-                if(me.input.isKeyPressed("attack")){
+        if(me.input.isKeyPressed("attack")){
             if(!this.renderable.isCurrentAnimation("attack")){
                 this.renderable.setCurrentAnimation("attack", "idle");
                 this.renderable.setAnimationFrame();
             }
         }        
-        else if(this.body.vel.x !== 0){
+        else if(this.body.vel.x !== 0 && !this.renderable.isCurrentAnimation("attack")){
         if(!this.renderable.isCurrentAnimation("walk")){
             this.renderable.setCurrentAnimation("walk");
         
@@ -58,11 +63,36 @@ game.PlayerEntity = me.Entity.extend({
         }
         
 
-        
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
         this.body.update(delta);
         
         this._super(me.Entity, "update", [delta]);
         return true;
+    },
+    
+    collideHandler: function(response) {
+        if(response.b.type==='EnemyBaseEntity'){
+            var ydif = this.pos.y - response.b.pos.y;
+            var xdif = this.pos.x -response.b.pos.x;
+            
+            if(ydif<-40 && xdif< 70 && xdif>-35){
+                this.body.falling = false;
+                this.body.vel.y = -1;
+            }
+            else if(xdif>-35 && this.facing==='right' && (xdif<0) && ydif>-50) {
+                this.body.vel.x = 0;
+                this.pos.x = this.pos.x -1;
+            }
+            else if(xdif<70 && this.facing==='left' && (xdif>0)) {
+                this.body.vel.x = 0;
+                this.pos.x = this.pos.x +1;
+            }
+            
+            if(this.rederable.isCurrentAnimation("attack") && this.now-this.lastHit >= 800){
+                this.lastHit = this.now; 
+                response.b.loseHealth();
+            }
+        }
     }
 });
 
@@ -124,7 +154,7 @@ game.EnemyBaseEntity = me.Entity.extend({
     this.alwaysUpdate = true;
     this.body.onCollision = this.onCollision.bind(this);
     
-    this.type = "PlayerBaseEntity";
+    this.type = "EnemyBaseEntity";
     
     this.renderable.addAnimation("idle", [0]);
     this.renderable.addAnimation("broken", [1]);
@@ -145,6 +175,10 @@ game.EnemyBaseEntity = me.Entity.extend({
     
     onCollision: function() {
         
+    },
+    
+    loseHealth: function() {
+        this.health--;
     }
     
 });
